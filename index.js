005d8de8fa8363975939ca65e03f7d427114c07a
LOMASH RISHI <103207945+lomashrishi@users.noreply.github.com>
2024-02-20 11:39:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Define a route to render the HTML page
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
